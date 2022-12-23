121567fa5acc4ae5b392fcb75247137c750b847b
AnasJMirza <ajmhimself@gmail.com>
2022-12-24 02:45:16 +0500

    login-page setup

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,9 +1,28 @@
 import React from 'react';
 
+import loginBg from '../assets/videos/login-bg.mp4';
+import snappyLogo from '../assets/images/snappy-logo.png';
+
 const Login = () => {
     return (
-        <div className='text-3xl'>
-            Login
+        <div className='flex flex-col justify-start items-center h-screen'>
+            <div className='relative h-full w-full'>
+                <video 
+                src={loginBg}
+                autoPlay
+                muted
+                loop
+                controls={false}
+                type='video/mp4'
+                className='w-full h-full object-cover'
+                />
+            </div>
+
+            <div className='absolute h-full w-full top-0 bottom-0 right-0 left-0 bg-blackOverlay flex justify-center items-center'>
+                <div>
+                    <img src={snappyLogo} className='h-8'/>
+                </div>
+            </div>
         </div>
     );
 };
